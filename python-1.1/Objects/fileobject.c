ddec5f8ceaabfc57aaea2900fa6a5337305a1170
Anthony Sottile <asottile@umich.edu>
2016-03-27 09:41:13 -0700

    bash -c "cd python-1.1 && git show 43eef814b79c1b7d4a58f8e924f9059d37d512f0 | patch -p2"

--- python-1.1/Objects/fileobject.c
+++ python-1.1/Objects/fileobject.c
@@ -361,7 +361,7 @@ file_read(f, args)
 */
 
 static object *
-getline(f, n)
+py_getline(f, n)
 	fileobject *f;
 	int n;
 {
@@ -482,7 +482,7 @@ filegetline(f, n)
 	}
 	if (((fileobject*)f)->f_fp == NULL)
 		return err_closed();
-	return getline((fileobject *)f, n);
+	return py_getline((fileobject *)f, n);
 }
 
 /* Python method */
@@ -507,7 +507,7 @@ file_readline(f, args)
 		}
 	}
 
-	return getline(f, n);
+	return py_getline(f, n);
 }
 
 static object *
@@ -525,7 +525,7 @@ file_readlines(f, args)
 	if ((list = newlistobject(0)) == NULL)
 		return NULL;
 	for (;;) {
-		line = getline(f, 0);
+		line = py_getline(f, 0);
 		if (line != NULL && getstringsize(line) == 0) {
 			DECREF(line);
 			break;
