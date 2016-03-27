476fe8f0e47808ee8970c275e66b8efe38c3fae9
Anthony Sottile <asottile@umich.edu>
2016-03-26 17:29:40 -0700

    Rename getline to py_getline (clash with stdio.h) -- similar to 43eef814b79c1b7d4a58f8e924f9059d37d512f0

--- python-1.5.2/Objects/fileobject.c
+++ python-1.5.2/Objects/fileobject.c
@@ -587,7 +587,7 @@ file_readinto(f, args)
 */
 
 static PyObject *
-getline(f, n)
+py_getline(f, n)
 	PyFileObject *f;
 	int n;
 {
@@ -709,7 +709,7 @@ PyFile_GetLine(f, n)
 	}
 	if (((PyFileObject*)f)->f_fp == NULL)
 		return err_closed();
-	return getline((PyFileObject *)f, n);
+	return py_getline((PyFileObject *)f, n);
 }
 
 /* Python method */
@@ -729,7 +729,7 @@ file_readline(f, args)
 		return PyString_FromString("");
 	if (n < 0)
 		n = 0;
-	return getline(f, n);
+	return py_getline(f, n);
 }
 
 static PyObject *
@@ -823,7 +823,7 @@ file_readlines(f, args)
 			goto error;
 		if (sizehint > 0) {
 			/* Need to complete the last line */
-			PyObject *rest = getline(f, 0);
+			PyObject *rest = py_getline(f, 0);
 			if (rest == NULL) {
 				Py_DECREF(line);
 				goto error;
